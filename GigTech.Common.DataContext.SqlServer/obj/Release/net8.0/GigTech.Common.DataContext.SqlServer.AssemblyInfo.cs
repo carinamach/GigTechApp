5b9849376a9213fb79f22ade8990c5b19d7a00f5
carinamach <carina.mach1999@gmail.com>
2024-03-29 02:28:41 +0100

    filstruktur

--- GigTech.Common.DataContext.SqlServer/obj/Release/net8.0/GigTech.Common.DataContext.SqlServer.AssemblyInfo.cs
+++ GigTech.Common.DataContext.SqlServer/obj/Release/net8.0/GigTech.Common.DataContext.SqlServer.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("GigTech.Common.DataContext.SqlServer")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30e62c2adfdbf684144f29f9ccc5022c5f324175")]
 [assembly: System.Reflection.AssemblyProductAttribute("GigTech.Common.DataContext.SqlServer")]
--- GigTech.Common.EntityModels.SqlServer/obj/Debug/net8.0/GigTech.Common.EntityModels.SqlServer.AssemblyInfo.cs
+++ GigTech.Common.EntityModels.SqlServer/obj/Debug/net8.0/GigTech.Common.EntityModels.SqlServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GigTech.Common.EntityModels.SqlServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30e62c2adfdbf684144f29f9ccc5022c5f324175")]
 [assembly: System.Reflection.AssemblyProductAttribute("GigTech.Common.EntityModels.SqlServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GigTech.Common.EntityModels.SqlServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
