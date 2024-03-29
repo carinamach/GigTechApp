7804cebbcdf318b312ba538286cf53e82539a36c
carinamach <carina.mach1999@gmail.com>
2024-03-29 02:45:41 +0100

    e

--- GigTechMvc/Program.cs
+++ GigTechMvc/Program.cs
@@ -21,7 +21,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
 }
-else 
+else
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
