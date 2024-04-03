eaad8e65c8f3e35d9433de7387672dc75ca4e130
ma224ip <mostafa1996@live.se>
2024-04-03 13:26:49 +0200

    dine

--- GigTech.Common.DataContext.SqlServer/obj/Debug/net8.0/GigTech.Common.DataContext.SqlServer.AssemblyInfo.cs
+++ GigTech.Common.DataContext.SqlServer/obj/Debug/net8.0/GigTech.Common.DataContext.SqlServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GigTech.Common.DataContext.SqlServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27425f5501494b9535a4a4d777992fbb1a85307d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55c1310de706f67ebcaf570334d47ea798ac26d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("GigTech.Common.DataContext.SqlServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GigTech.Common.DataContext.SqlServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
