4c31710846b570011364e758f3ce448fd8a260fe
carinamach <carina.mach1999@gmail.com>
2024-04-03 13:48:32 +0200

    .

--- GigTech.Common.DataContext.SqlServer/obj/Debug/net8.0/GigTech.Common.DataContext.SqlServer.AssemblyInfo.cs
+++ GigTech.Common.DataContext.SqlServer/obj/Debug/net8.0/GigTech.Common.DataContext.SqlServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GigTech.Common.DataContext.SqlServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a92a077764c65c7f00311c36da33907c7aa40bff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6d938ada79f589234dd8ef3f0daca9dea94b3f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("GigTech.Common.DataContext.SqlServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GigTech.Common.DataContext.SqlServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
