b8ba891a839d93d6520cdaffc847fc20f75edf95
Sia <seattledraws+github@gmail.com>
2024-04-03 10:46:04 +0200

    Files: Added my Support pages. Added IdentityContext<IdentityDbContext>. File changes: Program.cs: Added configuration function. added IdentityContext as DbContext for logins. Appsettings: Added secondary connection string. GigTechContext: removed conflicting configurations. Database: Applied migrations for login functionality to secondary login database.

--- GigTech.Common.DataContext.SqlServer/GigTechContext.cs
+++ GigTech.Common.DataContext.SqlServer/GigTechContext.cs
@@ -15,18 +15,6 @@ public partial class GigTechContext : DbContext
     {
     }
 
-    public virtual DbSet<AspNetRole> AspNetRoles { get; set; }
-
-    public virtual DbSet<AspNetRoleClaim> AspNetRoleClaims { get; set; }
-
-    public virtual DbSet<AspNetUser> AspNetUsers { get; set; }
-
-    public virtual DbSet<AspNetUserClaim> AspNetUserClaims { get; set; }
-
-    public virtual DbSet<AspNetUserLogin> AspNetUserLogins { get; set; }
-
-    public virtual DbSet<AspNetUserToken> AspNetUserTokens { get; set; }
-
     public virtual DbSet<Customer> Customers { get; set; }
 
     public virtual DbSet<ForumPost> ForumPosts { get; set; }
@@ -47,31 +35,6 @@ public partial class GigTechContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<AspNetRole>(entity =>
-        {
-            entity.HasIndex(e => e.NormalizedName, "RoleNameIndex")
-                .IsUnique()
-                .HasFilter("([NormalizedName] IS NOT NULL)");
-        });
-
-        modelBuilder.Entity<AspNetUser>(entity =>
-        {
-            entity.HasIndex(e => e.NormalizedUserName, "UserNameIndex")
-                .IsUnique()
-                .HasFilter("([NormalizedUserName] IS NOT NULL)");
-
-            entity.HasMany(d => d.Roles).WithMany(p => p.Users)
-                .UsingEntity<Dictionary<string, object>>(
-                    "AspNetUserRole",
-                    r => r.HasOne<AspNetRole>().WithMany().HasForeignKey("RoleId"),
-                    l => l.HasOne<AspNetUser>().WithMany().HasForeignKey("UserId"),
-                    j =>
-                    {
-                        j.HasKey("UserId", "RoleId");
-                        j.ToTable("AspNetUserRoles");
-                        j.HasIndex(new[] { "RoleId" }, "IX_AspNetUserRoles_RoleId");
-                    });
-        });
 
         modelBuilder.Entity<Customer>(entity =>
         {
---
+++ GigTechMvc/Data/IdentityContext.cs
@@ -0,0 +1,22 @@
+﻿using GigTech.Shared;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
+
+namespace GigTechMvc.Data
+{
+    public class IdentityContext : IdentityDbContext<IdentityUser, IdentityRole, string>
+    {
+        public IdentityContext(DbContextOptions<IdentityContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<IdentityRole>().ToTable("AspNetRoles");
+        }
+    }
+}
