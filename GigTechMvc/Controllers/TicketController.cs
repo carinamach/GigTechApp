a4aeeb3aceea9aa95cff59ea1d09b09433ec66c8
Sia <seattledraws+github@gmail.com>
2024-04-04 11:52:35 +0200

    Pruned

--- GigTechMvc/Controllers/TicketController.cs
+++ GigTechMvc/Controllers/TicketController.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using GigTech.Shared;
-using System.Drawing.Text;
-using static GigTechMvc.Controllers.SupportController;
 
 namespace GigTechMvc.Controllers
 {
