57dc20d1f5e7a341f9ab1e60e893338b1557957a
carinamach <carina.mach1999@gmail.com>
2024-04-10 20:25:29 +0200

    .

--- GigTechMvc/Controllers/ForumController.cs
+++ GigTechMvc/Controllers/ForumController.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GigTech.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace GigTechMvc.Controllers
 {
     public class ForumController : Controller
     {
+        private readonly GigTechContext _context;
         public IActionResult ForumIndex()
         {
             return View("/Views/Pages/ForumPage.cshtml");
@@ -11,6 +14,26 @@ namespace GigTechMvc.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult SaveText(string title, string content)
+        {
+            if (!string.IsNullOrEmpty(title) && !string.IsNullOrEmpty(content))
+            {
+                var forumPost = new ForumPost()
+                {
+                    Title = title,
+                    Content = content,
+                    CreationDate = DateTime.Now,
+                    CustomerId = 1, // Assuming you have some authentication system to get the current user ID
+                    ThreadId = 2, // Assuming you have a way to specify the thread ID
+                };
+
+                _context.ForumPosts.Add(forumPost);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("ForumPrivacy");
         }
     }
 }
