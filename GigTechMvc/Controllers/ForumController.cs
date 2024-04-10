bf6fc77395f62878f26b3a8a32e4fbc611293838
carinamach <carina.mach1999@gmail.com>
2024-04-10 19:35:18 +0200

    recreate forum branch

---
+++ GigTechMvc/Controllers/ForumController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GigTechMvc.Controllers
+{
+    public class ForumController : Controller
+    {
+        public IActionResult ForumIndex()
+        {
+            return View("/Views/Pages/ForumPage.cshtml");
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
